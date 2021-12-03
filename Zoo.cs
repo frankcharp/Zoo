5e50eda5b48a91a3220a0acd1923ec8ba835dcc9
vlad <vladyorkyee@gmail.com>
2021-12-03 15:54:08 +0300

    Welcome

---
+++ Zoo.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ConsoleTest5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ZOO");
+        }
+    }
+}
